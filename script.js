7cb17522ecfd4e44daa227e4ab52d4173083734e
Richard <richardzou25@gmail.com>
2024-10-10 11:58:01 -0400

    Modified existing functions
    Split playRound and playGame functions separately,
    so each can be accessed.
    
    Moved score keeping variables outside of functions,
    so they are globally accessible.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // const rockButton = document.getElementById("rock-button");
 // rockButton.addEventListener('click', function playRound() {
 // });
-
+let playerScore = 0;
+let computerScore = 0;
 function getComputerChoice() {
     let choice = [Math.floor(Math.random() * 3)];
     if (choice == 0) {
@@ -20,30 +21,31 @@ function getHumanChoice() {
 
 
 function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    playRound();
-    function playRound(humanChoice, computerChoice) {
-
-        if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "paper" && computerChoice == "rock" || humanChoice == "scissors" && computerChoice == "paper") {
-            console.log("You Win!");
-            humanScore++;
-        } else if (humanChoice == computerChoice) {
-            console.log("It's a tie!");
-        } else {
-            console.log("You lose!");
-            computerScore++;
-        };
-        return computerScore
-    }
     for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
+        playRound(getHumanChoice());
     }
-    if (computerScore > humanScore) {
+    if (computerScore > playerScore) {
         console.log("You lost the game")
+    } else if (computerScore === playerScore) {
+        console.log("It's a tie!")
     } else {
         console.log("You win the game!")
-    }
+    };
+}
+
+function playRound(humanChoice) {
+    let computerChoice = getComputerChoice();
+    if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "paper" && computerChoice == "rock" || humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You Win!");
+        playerScore++;
+    } else if (humanChoice == computerChoice) {
+        console.log("It's a tie!");
+    } else {
+        console.log("You lose!");
+        computerScore++;
+    };
+    return computerScore;
+    return playerScore;
 }
 
 playGame();
